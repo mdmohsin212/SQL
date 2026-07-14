32b2d3d1638877aa2fd8205fb902e65224e023d6
md mohsin <siam.mohsin2005@gmail.com>
2026-07-14 20:34:37 +0600

    basic sql

--- Basic_sql.sql
+++ Basic_sql.sql
@@ -11,3 +11,29 @@ SELECT
     salary AS monthly_salary
 FROM employees;
 
+SELECT full_name, job_role
+FROM employees
+WHERE city = "Dhaka";
+
+SELECT full_name, job_role
+FROM employees
+WHERE salary >= 50000;
+
+SELECT full_name, department, salary
+FROM employees
+WHERE department = 'AI'
+    AND salary >= 50000;
+
+SELECT full_name, city
+FROM employees
+WHERE city = "Dhaka"
+    OR city = "Chattogram";
+
+SELECT full_name, city
+FROM employees
+WHERE city <> 'Dhaka';
+
+SELECT full_name, salary
+FROM employees
+WHERE salary BETWEEN 30000 AND 60000;
+
