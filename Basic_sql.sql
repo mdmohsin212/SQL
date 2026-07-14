9e301554557598a0b12129977f6245acd1374d8b
md mohsin <siam.mohsin2005@gmail.com>
2026-07-14 20:16:30 +0600

    Basic sql query

---
+++ Basic_sql.sql
@@ -0,0 +1,13 @@
+USE sql_interview_stage1;
+
+SELECT * FROM employees;
+
+SELECT full_name, job_role, salary
+FROM employees;
+
+SELECT
+    full_name AS employee_name,
+    job_role AS role,
+    salary AS monthly_salary
+FROM employees;
+
